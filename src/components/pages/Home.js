6238b032e999782ec4f3645f2deaac28a6074695
Josh Rehanek <joshrehanek@gmail.com>
2021-04-06 13:44:28 -0700

    adds home page

---
+++ src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../../styles.css";
+import Hero from "../Hero/Hero";
+
+export default function Home () {
+    return (
+        <div>
+            <Hero />
+        </div>
+    )
+}
+
