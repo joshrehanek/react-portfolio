b6404922d45ce888e0788620087008ed512285b9
Josh Rehanek <joshrehanek@gmail.com>
2021-04-06 12:46:57 -0700

    adds app Router, Nav, Switch, and Route to Home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import "./styles.css";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Nav from "./components/Navbar/Nav"
+import Home from "./components/pages/Home"
 
 export default function App() {
   return (
-    <div>
-      
+    <div className="App">
+      <Router>
+        <Nav />
+        <Switch>
+          <Route path="/" exact component={Home}/>
+        </Switch>
+      </Router>
+
     </div>
   )
 }
