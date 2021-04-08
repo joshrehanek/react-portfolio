56968d95813b6290f424fd29561a2fd164f42787
Josh Rehanek <joshrehanek@gmail.com>
2021-04-07 23:05:05 -0700

    adds video bg & linkedin, github, resume links

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,35 +1,58 @@
 import React from "react";
 import "../../styles.css"
 import "./Hero.css"
-import { Button } from "../Button/Button";
-
+import PurpleGrid from "../../assets/videos/PurpleGrid.mp4"
 
 
 export default function Hero() {
     return (
         <div className="hero">
-            <video src="/videos/PurpleGrid.mp4" autoPlay loop />
-            <h1>Open-Minded Development</h1>
-            <div className="hero-btns">
-                <Button
-                    className="btns"
-                    // buttonStyle="btn-outline"
-                    buttonSize="btn-large">
-                    <i class="fab fa-github" />
-                </Button>
-                <Button
-                    className="btns"
-                    // buttonStyle="btn-outline"
-                    buttonSize="btn-large">
-                    <i class="fab fa-linkedin-in" />
-                </Button>
-                <Button
-                    className="btns"
-                    // buttonStyle="btn-outline"
-                    buttonSize="btn-large">
-                    <i class="fas fa-file">Resume</i>
-                </Button>
-            </div>
+            <video
+                autoPlay
+                loop
+                muted>
+                <source
+                    src={PurpleGrid}
+                    type="video/mp4" />
+            </video>
+            <section class="banner" id="home">
+                <div class="textBox">
+                    <h2>Welcome, I'm<br></br><span>Joshua Azzam Rehanek.</span> </h2>
+                    <h3>Full Stack Web Developer</h3>
+                <br>
+                </br>
+                <a
+                    class="link"
+                    href="https://www.linkedin.com/in/joshua-rehanek-a266a0200/"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    <img
+                        src="https://img.icons8.com/cute-clipart/64/000000/linkedin.png"
+                        alt="link to linkedin"
+                    />
+                </a>
+                <a
+                    class="link"
+                    href="https://github.com/joshrehanek" target="_blank"
+                    rel="noreferrer"
+                >
+                    <img
+                        src="https://img.icons8.com/dusk/64/000000/github.png"
+                        alt="link to github" />
+                </a>
+                <a
+                    class="link"
+                    href="./assets/images/Resume.pdf"
+                    download="./assets/images/Resume.pdf"
+                >
+                    <img
+                        src="https://img.icons8.com/nolan/64/open-resume.png"
+                        alt="download resume" />
+                </a>
+                </div>
+            </section>
+            
         </div>
     )
 }
