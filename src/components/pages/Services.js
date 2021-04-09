884614cb497232494b6814c5a07950cc76a93c8f
Josh Rehanek <joshrehanek@gmail.com>
2021-04-08 18:27:08 -0700

    initial commit

---
+++ src/components/pages/Services.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../styles.css'
+
+export default function Services() {
+    return (
+        <div>
+            <h1 className="services">Services</h1>
+        </div>
+    )
+}
