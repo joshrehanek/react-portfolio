38a6bd944d196f32a3ca3fab84e90c1d1e9bded7
Josh Rehanek <joshrehanek@gmail.com>
2021-04-08 18:27:46 -0700

    adds Cards component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import "../../styles.css";
+import Cards from '../Cards/Cards';
 import Hero from "../Hero/Hero";
 
 export default function Home () {
     return (
         <div>
             <Hero />
+            <Cards />
         </div>
     )
 }
