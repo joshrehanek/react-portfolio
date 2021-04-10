ee9977951ae945d92555f2a67c58d5f1faebaf10
Josh Rehanek <joshrehanek@gmail.com>
2021-04-10 01:45:06 -0700

    adds footer component

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import "../../styles.css";
 import Cards from '../Cards/Cards';
 import Hero from "../Hero/Hero";
+import Footer from "../Footer/Footer"
 
 export default function Home () {
     return (
         <div>
             <Hero />
             <Cards />
+            <Footer />
         </div>
     )
 }
