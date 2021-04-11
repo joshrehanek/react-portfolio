c08f9ea92eb5ac1601d17ec36ecd7f9d4d76130e
Josh Rehanek <joshrehanek@gmail.com>
2021-04-10 21:11:00 -0700

    links section to nav; format cleaning

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -14,71 +14,71 @@ import './Cards.css';
 
 export default function Cards() {
     return (
-    <div className="cards">
-        <h2>Services</h2>
-        <Container className="cards-container">
-            <Row>
-                <Col>
-                    <ServicesCard 
-                        src={AccessibilityImg}
-                        text="Websites made with a focus on accessibility for all types of users."
-                        label="Accessibility"
-                        path="/services" />
-                </Col>
-                <Col>             
-                    <ServicesCard 
-                        src= {FullStack}
-                        text= "Fluent in:  React, Javascript, Node, Express, Mongoose, MySQL, Sequelize, JQuery, HTML, CSS,  Bootstrap, Foundation, Git, AJAX, and more."
-                        label="Full-Stack Web Development"
-                        path="/services"
-                    />
-                </Col>
-                <Col>    
-                    <ServicesCard
-                    src={WebDesign}
-                    text= "Modern, stylish, & functional websites that are intuitive & easy to use."
-                    label="Web Design || UI/UX"
-                    path="/services"  />
-                </Col>
-            </Row>
-        </Container>
-            <h2>Projects</h2>
+        <div id="services" className="cards">
+            <h2>Services</h2>
             <Container className="cards-container">
-            <Row >
-                <Col>     
-                    <ProjectCard
-                    src={Recipes}
-                    text="A full stack web Recipe app using Javascript, HTML, CSS, Node, Express, & Sequelize."
-                    path="/projects"
-                    href="https://github.com/jinxdoll/my-favorite-recipes-app"
-                    />
-                </Col>
-                <Col>      
-                    <ProjectCard
-                    src={WeatherApp}
-                    text="A front end weather app using React, Javascript, HTML, CSS, React Bootstrap, & Node."
-                    path="/projects"
-                    href="https://github.com/joshrehanek/react-weather-app"
-                    />
-                </Col>
-                <Col>
-                    <ProjectCard
-                    src={BarTrivia}
-                    text="A front end Bar Trivia Simulator using Javascript, HTML, CSS, Foundation, TheCocktailDB API, & Open Trivia Database API."
-                    path="/projects"
-                    href="https://github.com/joshrehanek/bar-trivia-simulator"
-                    />
-                </Col>
-                <Col>
-                    <ProjectCard
-                    src={Fitness}
-                    text="A full stack fitness tracker using Javascript, HTML, CSS, Node, Express, & Mongoose."
-                    path="/projects"
-                    href="https://github.com/joshrehanek/fitness-tracking-app"
-                    />
-                </Col>
-            </Row>
-        </Container>
-    </div>
+                <Row>
+                    <Col>
+                        <ServicesCard
+                            src={AccessibilityImg}
+                            text="Websites made with a focus on accessibility for all types of users."
+                            label="Accessibility"
+                            path="/services" />
+                    </Col>
+                    <Col>
+                        <ServicesCard
+                            src={FullStack}
+                            text="Fluent in:  React, Javascript, Node, Express, Mongoose, MySQL, Sequelize, JQuery, HTML, CSS,  Bootstrap, Foundation, Git, AJAX, and more."
+                            label="Full-Stack Web Development"
+                            path="/services"
+                        />
+                    </Col>
+                    <Col>
+                        <ServicesCard
+                            src={WebDesign}
+                            text="Modern, stylish, & functional websites that are intuitive & easy to use."
+                            label="Web Design || UI/UX"
+                            path="/services" />
+                    </Col>
+                </Row>
+            </Container>
+            <h2 id="projects">Projects</h2>
+            <Container className="cards-container">
+                <Row >
+                    <Col>
+                        <ProjectCard
+                            src={Recipes}
+                            text="A full stack web Recipe app using Javascript, HTML, CSS, Node, Express, & Sequelize."
+                            path="/projects"
+                            href="https://github.com/jinxdoll/my-favorite-recipes-app"
+                        />
+                    </Col>
+                    <Col>
+                        <ProjectCard
+                            src={WeatherApp}
+                            text="A front end weather app using React, Javascript, HTML, CSS, React Bootstrap, & Node."
+                            path="/projects"
+                            href="https://github.com/joshrehanek/react-weather-app"
+                        />
+                    </Col>
+                    <Col>
+                        <ProjectCard
+                            src={BarTrivia}
+                            text="A front end Bar Trivia Simulator using Javascript, HTML, CSS, Foundation, TheCocktailDB API, & Open Trivia Database API."
+                            path="/projects"
+                            href="https://github.com/joshrehanek/bar-trivia-simulator"
+                        />
+                    </Col>
+                    <Col>
+                        <ProjectCard
+                            src={Fitness}
+                            text="A full stack fitness tracker using Javascript, HTML, CSS, Node, Express, & Mongoose."
+                            path="/projects"
+                            href="https://github.com/joshrehanek/fitness-tracking-app"
+                        />
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };
