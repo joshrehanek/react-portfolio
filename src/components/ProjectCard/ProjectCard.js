9aa49592229b430df0b6b8a1abea8c1ca9dab913
Josh Rehanek <joshrehanek@gmail.com>
2021-05-06 11:25:34 -0700

    adds label to project cards

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,24 +1,24 @@
 import React from 'react';
+import { Card, Button } from "react-bootstrap";
+import "./ProjectCard.css"
 
-import {Card, Button} from "react-bootstrap";
-
-export default function ProjectCard({ path, src, text, href}) {
+export default function ProjectCard({ path, src, text, href, label }) {
     return (
         <>
             <Card style={{ width: '18rem' }}>
-                <Card.Img path={path} variant="top" src={src}/>
+                <Card.Img path={path} variant="top" src={src} />
                 <Card.Body>
-                    <Card.Text>
-                    {text}
+                    <Card.Text className="label">
+                        {label}
+                    </Card.Text>
+                    <Card.Text className="project-text">
+                        {text}
                     </Card.Text>
                     <a target="_blank" rel="noreferrer" href={href}>
-                    <Button variant="primary">
-                    Github
+                        <Button variant="primary">
+                            Github
                     </Button>
                     </a>
-
-                    
-                    
                 </Card.Body>
             </Card>
         </>
