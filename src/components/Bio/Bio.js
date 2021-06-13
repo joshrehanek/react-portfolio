0a88b888a4b5e49323bd8ef28c1816abc0feb82f
Josh Rehanek <joshrehanek@gmail.com>
2021-06-13 12:37:19 -0700

    adds bio compontent and content

---
+++ src/components/Bio/Bio.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Bio.css';
+
+export default function Bio() {
+    return (
+        <div id="bio" className="bio-container">
+                <div class="heading">
+                    <h2>Bio</h2>
+                </div>
+                <div>
+                    <h5 class="bio-content">I make Full-Stack web apps with an emphasis on accessability, fluidity, & flexability. Creating is my passion, having spent over a decade as a performer I get a rush from collaborating with other passionate individuals to create beautiful and intuitive applications for people from all walks of life.  </h5>                    
+                </div> 
+        </div>
+    );
+};
