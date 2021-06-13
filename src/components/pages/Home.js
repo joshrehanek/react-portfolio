17bca32c8442a00e74a3cec0068c51664ad04ca0
Josh Rehanek <joshrehanek@gmail.com>
2021-06-13 12:37:53 -0700

    adds bio component to Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,11 +3,13 @@ import "../../styles.css";
 import Cards from '../Cards/Cards';
 import Hero from "../Hero/Hero";
 import Footer from "../Footer/Footer"
+import Bio from "../Bio/Bio"
 
 export default function Home () {
     return (
         <div>
             <Hero />
+            <Bio />
             <Cards />
             <Footer />
         </div>
