0a0a9bb863ec4bbe8b9e8a9bf1368063c1cefe4b
Josh Rehanek <joshrehanek@gmail.com>
2021-07-22 18:51:36 -0700

    adds bootstrap card to hold new footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,49 +1,41 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
 import './Footer.css';
 
 export default function Footer() {
     return (
-        <div id="contact" className="footer-container">
-            <div className="footer-contact">
-                <div class="heading">
-                    <h2>Contact</h2>
+        <Card id='contact'>
+            <h2> Contact</h2>
+            <Card.Body class='footer-contact'>
+                <div class="icon">
+                    <i class="fas fa-street-view"></i>
                 </div>
-                <div class="content">
-                    <div class="contactInfo">
-                        <div class="contactInfoBox">
-                            <div class="box">
-                                <div class="icon">
-                                    <i class="fas fa-street-view"></i>
-                                </div>
-                                <div class="text">
-                                    <h3>Address</h3>
-                                    <p>5528 NE 14th Ave,
-                                    Portland, Oregon
-                                    97211</p>
-                                </div>
-                            </div>
-                            <div class="box">
-                                <div class="icon">
-                                    <i class="fas fa-phone-alt"></i>
-                                </div>
-                                <div class="text">
-                                    <h3>Phone</h3>
-                                    <p>918-361-6213</p>
-                                </div>
-                            </div>
-                            <div class="box">
-                                <div class="icon">
-                                    <i class="fas fa-envelope"></i>
-                                </div>
-                                <div class="text">
-                                    <h3>Email</h3>
-                                    <p>joshrehanek@gmail.com</p>
-                                </div>
-                            </div>
-                        </div>
+                <div class="text">
+                    <h3>Address</h3>
+                    <p>5528 NE 14th Ave,
+                        Portland, Oregon
+                        97211
+                    </p>
+                </div>
+                <div class="box">
+                    <div class="icon">
+                        <i class="fas fa-phone-alt"></i>
+                    </div>
+                    <div class="text">
+                        <h3>Phone</h3>
+                        <p>918-361-6213</p>
                     </div>
                 </div>
-            </div>
-        </div>
-    );
+                <div class="box">
+                    <div class="icon">
+                        <i class="fas fa-envelope"></i>
+                    </div>
+                    <div class="text">
+                        <h3>Email</h3>
+                        <p>joshrehanek@gmail.com</p>
+                    </div>
+                </div>
+            </Card.Body>
+        </Card>
+            );
 };
