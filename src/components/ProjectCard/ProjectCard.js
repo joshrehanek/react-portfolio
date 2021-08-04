1c95d7887b7785f689719fcc23bf373ecd9d20f6
Josh Rehanek <joshrehanek@gmail.com>
2021-08-04 14:15:35 -0700

    changes name to fit best practice

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,10 +1,11 @@
+//import dependencies
 import React from 'react';
 import { Card, Button } from "react-bootstrap";
 import "./ProjectCard.css"
 
 export default function ProjectCard({ path, src, text, href, href2, label }) {
     return (
-        <div className="projects">
+        <div className="project-card">
             <Card>
                 <Card.Img path={path} variant="top" src={src} />
                 <Card.Body>
