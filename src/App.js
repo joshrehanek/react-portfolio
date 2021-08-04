0ebf745517559ad47553ba5b90b315d992a0384a
Josh Rehanek <joshrehanek@gmail.com>
2021-08-04 13:36:15 -0700

    adds comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+//import dependencies
 import React from 'react'
 import "./styles.css";
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+//import components
 import Nav from "./components/Navbar/Nav"
 import Home from "./components/pages/Home"
 
@@ -9,6 +11,7 @@ import Home from "./components/pages/Home"
 export default function App() {
   return (
     <div className="App">
+    {/* uses react-router to set Navbar and switch path */}
       <Router>
         <Nav />
         <Switch>
