83ecb5ee249f9ff99be7afb21f581b690003cabd
Josh Rehanek <joshrehanek@gmail.com>
2021-08-04 13:50:55 -0700

    code cleanup and comments

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -1,3 +1,4 @@
+//import dependencies
 import React from 'react';
 import './Bio.css';
 import { Container, Row, Col } from 'react-bootstrap';
@@ -13,12 +14,12 @@ export default function Bio() {
                             <h2>Bio</h2>
                         </div>
                         <div>
-                            <h5 class="bio-content">I make Full-Stack web apps with an emphasis on accessability, fluidity, & flexability. Creating is my passion, having spent over a decade as a performer I get a rush from collaborating with other passionate individuals to create beautiful and intuitive applications for people from all walks of life.  
+                            <h5>
+                            I make Full-Stack web apps with an emphasis on accessability, fluidity, & flexability. Creating is my passion, having spent over a decade as a performer I get a rush from collaborating with other passionate individuals to create beautiful and intuitive applications for people from all walks of life.  
                             </h5>
-                            <br>
-
-                            </br>
-                            <h5 class="bio-content">
+                            <br />
+                            
+                            <h5>
                             I received my certification in Full Stack Web Development from the University of Oregon where I worked in depth with JavaScropt, HTML5, CSS3, React, Node.js, Express.js, JQuery, Bootstrp, MongoDB, MySQL, Handlebars, CLI, Git, Database Theory, and more.During that time I was able to hone my programming skills to write clean, readable, and efficient code that finds elegant solutions.  I have ample experience working in a group setting and my excellent communication skills and high level of self-accountability have helped to serve every group setting I’ve worked in.
                             </h5>
                         </div>
